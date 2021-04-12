a3b19b7c40f6329a9adc95131d421e3f74c954c4
wealthralph <wealthralph@gmail.com>
2021-04-12 14:35:14 +0100

    Button component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import './navbar.css'
+import './navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -23,6 +23,38 @@ function Navbar() {
                 Home
               </Link>
             </l1>
+
+            <l1 className='nav-item'>
+              <Link to='/glasses' className='nav-links'>
+                Glasses
+              </Link>
+            </l1>
+
+            <l1 className='nav-item'>
+              <Link to='/order' className='nav-links'>
+                Order
+              </Link>
+            </l1>
+
+            <l1 className='nav-item'>
+              <Link to='/products' className='nav-links'>
+                Products
+              </Link>
+            </l1>
+
+            <li className='nav-btn'>
+              {button ? (
+                <Link className='nav-btn'>
+                  <Button buttonStyle='btn__outline'>Order Now</Button>
+                </Link>
+              ) : (
+                <Link className='btn-link'>
+                  <Button buttonStyle='btn--outline' ButtonSize='btn--mobile'>
+                    Sign Up
+                  </Button>
+                </Link>
+              )}
+            </li>
           </ul>
         </div>
       </div>
